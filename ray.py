7d2f1a04b0b49f6e66141ec6b719ee66724c2f5a
origanoinitalian <avishanabdinejad@gmail.com>
2025-12-27 12:10:51 +0100

    refactor to use hittable

--- ray.py
+++ ray.py
@@ -1,4 +1,5 @@
-from vec3 import vec3, point3, color, unit_vector
+from vec3 import vec3, point3, color, unit_vector, dot
+from hittable import hit_record
 
 class Ray:
     def __init__(self, origin, direction):
@@ -7,12 +8,27 @@ class Ray:
 
     def at(self, t):
         return self.orig + (self.direction_vec * t)
+    
 
-def ray_color(r):
-    # Use the unit_vector function you defined in vec3.py
+def hit_sphere(center, radius, r):
+    oc = r.orig - center
+    a = dot(r.direction_vec, r.direction_vec)
+    b = 2.0 * dot(oc, r.direction_vec)
+    c = dot(oc, oc) - radius*radius
+    discriminant = b*b - 4*a*c
+    return (discriminant > 0)
+
+def ray_color(r, world):
+
+    rec = hit_record()
+
+    if world.hit(r, 0.001, float('inf'), rec):
+        # Simple normal-based coloring
+        return 0.5 *(rec.normal +color(1, 0, 0)) 
+    
+    # if no hits, return sky gradient
     unit_direction = unit_vector(r.direction_vec)
     
-    # Use .y WITHOUT () because of your @property
     a = 0.5 * (unit_direction.y + 1.0)
     
     # This is the "Lerp" (White to Blue)
