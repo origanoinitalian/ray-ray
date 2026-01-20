4c2ae1280dfe8ce2c464c077151fa1e27012df24
origanoinitalian <avishanabdinejad@gmail.com>
2026-01-20 17:35:02 +0100

    feat:align with u,v

--- sphere.py
+++ sphere.py
@@ -8,7 +8,15 @@ class Sphere:
         self.radius = radius
         self.material = material
 
-    def hit(self, r, t_min, t_max, rec):
+    @staticmethod
+    def get_sphere_uv(p):
+        theta = math.acos(-p.y)
+        phi = math.atan2(-p.z, p.x) + math.pi
+        u = phi / (2 * math.pi)
+        v = theta / math.pi
+        return u, v
+
+    def hit(self, r, ray_t, rec):
         oc = r.orig - self.center
         a = r.direction_vec.length_squared()
         h = dot(r.direction_vec, oc)
@@ -21,14 +29,15 @@ class Sphere:
         sqrtd = math.sqrt(discriminant)
         # Find the nearest root in the acceptable range
         root = (-h - sqrtd) / a
-        if root <= t_min or t_max <= root:
+        if not ray_t.surrounds(root):
             root = (-h + sqrtd) / a
-            if root <= t_min or t_max <= root:
+            if not ray_t.surrounds(root):
                 return False
 
-        rec.t = root
-        rec.p = r.at(rec.t)
+        rec.t = root # Distance along ray
+        rec.p = r.at(rec.t)  # coordinates of Point of intersection
         rec.material = self.material
-        outward_normal = (rec.p - self.center) / self.radius
+        outward_normal = (rec.p - self.center) / self.radius # Normal vector at intersection
         rec.set_face_normal(r, outward_normal)
+        rec.u, rec.v = Sphere.get_sphere_uv(outward_normal) # Texture cmapping 3D to 2D
         return True
