9036c14140635ace2458ac21dc0079574af5f4db
origanoinitalian <avishanabdinejad@gmail.com>
2026-01-20 17:18:22 +0100

    feat:add u and v to hittable bc Quad has them

--- hittable.py
+++ hittable.py
@@ -1,10 +1,17 @@
 from vec3 import dot
+from interval import interval
+
+class hittable:     #abstraction to say quad is hittable
+    def hit(self, r, ray_t, rec):
+        return False
 
 class hit_record:
     def __init__(self):
         self.p = None       # Point of intersection
         self.normal = None  # Normal vector at intersection
         self.t = 0          # Distance along ray
+        self.u = 0          # Texture coordinate u
+        self.v = 0          # Texture coordinate v
         self.front_face = False
         self.material = None  
 
@@ -20,18 +27,20 @@ class hittable_list:
     def add(self, obj):
         self.objects.append(obj)
 
-    def hit(self, r, t_min, t_max, rec):
+    def hit(self, r, ray_t, rec):
         temp_rec = hit_record()
         hit_anything = False
-        closest_so_far = t_max
+        closest_so_far = ray_t.max
 
         for obj in self.objects:
-            if obj.hit(r, t_min, closest_so_far, temp_rec):
+            if obj.hit(r, interval(ray_t.min, closest_so_far), temp_rec):
                 hit_anything = True
                 closest_so_far = temp_rec.t
                 # Copy the temp_rec values to our main record
                 rec.t = temp_rec.t
                 rec.p = temp_rec.p
+                rec.u = temp_rec.u
+                rec.v = temp_rec.v
                 rec.normal = temp_rec.normal
                 rec.front_face = temp_rec.front_face
                 rec.material = temp_rec.material
